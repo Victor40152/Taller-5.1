6ddb378269c104efdbebbd175b495a2cbe3098ca
Victor Manuel Echavarria <vicme93@gmail.com>
2019-09-13 19:59:39 -0500

    Taller numeros impares

---
+++ Numeros impares/Form1.Designer.cs
@@ -0,0 +1,110 @@
+﻿namespace Numeros_impares
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.btnsumar = new System.Windows.Forms.Button();
+            this.txtsumar = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Arial", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(136, 18);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(215, 24);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Programa para Sumar";
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(117, 113);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(144, 121);
+            this.listBox1.TabIndex = 1;
+            // 
+            // btnsumar
+            // 
+            this.btnsumar.Location = new System.Drawing.Point(404, 67);
+            this.btnsumar.Name = "btnsumar";
+            this.btnsumar.Size = new System.Drawing.Size(75, 23);
+            this.btnsumar.TabIndex = 2;
+            this.btnsumar.Text = "Sumar";
+            this.btnsumar.UseVisualStyleBackColor = true;
+            this.btnsumar.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // txtsumar
+            // 
+            this.txtsumar.Location = new System.Drawing.Point(244, 67);
+            this.txtsumar.Multiline = true;
+            this.txtsumar.Name = "txtsumar";
+            this.txtsumar.Size = new System.Drawing.Size(107, 23);
+            this.txtsumar.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Arial", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(39, 67);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(177, 22);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Ingrese su Numero:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(520, 262);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtsumar);
+            this.Controls.Add(this.btnsumar);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button btnsumar;
+        private System.Windows.Forms.TextBox txtsumar;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ Numeros impares/Form1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Numeros_impares
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int i, impar = 0;
+            for (i=0; i <= Convert.ToInt16(txtsumar.Text);i++)
+            {
+                if (i%2 !=0)
+                {
+                    impar += i;
+                    listBox1.Items.Add(i.ToString());
+
+                }
+            }
+
+            MessageBox.Show("El Resultado es: " +impar);
+
+
+        }
+
+    }
+}
